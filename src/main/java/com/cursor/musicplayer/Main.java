34d1158563ed7a0ae4d0ef60f6cdd1a20a76c618
Michu474 <magamba474@gmail.com>
2022-10-27 14:44:03 +0300

    v2

--- src/main/java/com/cursor/musicplayer/Main.java
+++ src/main/java/com/cursor/musicplayer/Main.java
@@ -8,7 +8,6 @@ import java.lang.module.Configuration;
 public class Main {
     public static void main(String[] args) {
 
-//        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ProjectConfig.class);
 
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 "applicationContext.xml"
